8a780b5f31082ff9927e0d4047a2ba6c5fcae4cc
Yachinverma <yachinverma@gmail.com>
2025-06-18 15:48:09 +0530

    Backend ready

---
+++ routes/donorRoutes.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+const router = express.Router();
+const Donor = require('../models/Donor');
+
+// POST route to handle form data
+router.post('/donate', async (req, res) => {
+  try {
+    const newDonor = new Donor(req.body);
+    await newDonor.save();
+    res.status(201).json({ message: 'Donation info saved successfully!' });
+  } catch (error) {
+    console.error('Error saving data:', error);
+    res.status(500).json({ error: 'Failed to save data.' });
+  }
+});
+
+module.exports = router;
